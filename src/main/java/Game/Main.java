7aba8902231fdec0e896eb75bcf23a58ac8288b9
angeloid12 <32996078+angeloid12@users.noreply.github.com>
2021-11-11 15:31:11 +0200

    update1

--- src/main/java/Game/Main.java
+++ src/main/java/Game/Main.java
@@ -5,10 +5,8 @@ public class Main {
         Board board = new Board();
         Player playerOne = new RealPlayer("X", "Player One");
         Player playerTwo = new ComputerPlayer("0", "Player Two");
-        CurrentPlayer currentPlayer = new CurrentPlayer(playerOne, playerTwo);
-        Inputs inputs = new Inputs();
         BoardCheck boardCheck = new BoardCheck(board);
-        MakeATurn makeATurn = new MakeATurn(currentPlayer, board, inputs, boardCheck);
+        MakeATurn makeATurn = new MakeATurn(playerOne, playerTwo, board, boardCheck);
 
         makeATurn.runGame();
 
